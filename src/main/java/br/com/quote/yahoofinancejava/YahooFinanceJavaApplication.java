20eb1536d415eed0a0ddca849137d31e5f918f5a
matheusfranco1 <matheusfranco@arpiatecnologia.com.br>
2020-06-11 11:55:59 -0300

    Criando projeto da biblioteca de pegar cotação do yahoo finance

---
+++ src/main/java/br/com/quote/yahoofinancejava/YahooFinanceJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.quote.yahoofinancejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahooFinanceJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YahooFinanceJavaApplication.class, args);
+	}
+
+}
